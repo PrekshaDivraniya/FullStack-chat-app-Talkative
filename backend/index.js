f65a67f7dac5932bc9101ef1b3fa614b523b5b33
Preksha Divraniya <prekshadivraniya09@gmail.com>
2025-04-02 21:36:52 +0530

    Fix __dirname and update static file paths

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(
   })
 );
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve()
 
 app.use("/api/auth", authRoutes);
@@ -33,7 +33,7 @@ if(process.env.NODE_ENV === "production"){
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req,res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
   })
 }
 
